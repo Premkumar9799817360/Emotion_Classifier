7028f98e7c03e200a39d2eb811f3b52ce1d39653
PERM KUMAR <83695512+Premkumar9799817360@users.noreply.github.com>
2023-12-31 21:59:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pickle
 import pandas as pd 
 import numpy as np 
+import base64
 # import joblib 
 # pipe_lr = joblib.load(open("emotion_classifier.pkl","rb"))
 
@@ -42,3 +43,22 @@ if st.button('Predict'):
     else:
        st.title("🤗")
 st.header("Created By Prem Kumar")
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('image.jpg')
